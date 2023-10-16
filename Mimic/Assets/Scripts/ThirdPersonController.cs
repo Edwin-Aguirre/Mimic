20c82856e04c3aaba2669d9993d5ff0bed7aa503
Edwin-Aguirre <edwina3@uci.edu>
2023-10-15 18:52:58 -0700

    Fixed Animation Bug
    Fixed the player/enemy attack animation so that it now transitions off when they start to move.

--- Mimic/Assets/Scripts/ThirdPersonController.cs
+++ Mimic/Assets/Scripts/ThirdPersonController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f; // Default walking speed
+    public float moveSpeed = 5.0f;
     public Animator animator;
     private CharacterController characterController;
+
+    // New parameter to control the transition from attacking to idle
+    private bool isMoving = false;
 
     private void Start()
     {
@@ -29,17 +32,25 @@ public class ThirdPersonController : MonoBehaviour
         {
             // Set the character's rotation to face the direction of movement
             transform.rotation = Quaternion.LookRotation(moveDirection);
-
-            // Move the character at the specified speed
+            // Move the character
             characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
 
             // Set the animator parameter for walking
             animator.SetBool("isWalking", true);
+
+            // Player is moving, so disable the IsMoving parameter
+            isMoving = true;
         }
         else
         {
             // No movement, set the character to idle
             animator.SetBool("isWalking", false);
+
+            // Player is not moving, so enable the IsMoving parameter
+            isMoving = false;
         }
+
+        // Set the animator parameter for IsMoving to control the transition
+        animator.SetBool("isMoving", isMoving);
     }
 }
