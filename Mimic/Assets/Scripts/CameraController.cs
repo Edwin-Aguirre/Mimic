73f226e1f98bc76776bccfc2bc417b1ec95f5934
Edwin-Aguirre <edwina3@uci.edu>
2023-10-17 17:48:06 -0700

    Updated Mirror Mechanic and added Cinemachine

---
+++ Mimic/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera; // Reference to your Cinemachine Virtual Camera
+
+    private void Start()
+    {
+        // Start with no target
+        virtualCamera.Follow = null;
+        virtualCamera.LookAt = null;
+    }
+
+    private void Update()
+    {
+        // During gameplay, continuously check for the GameObject with the "Player" tag
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            // Assign the "Follow" and "Look At" targets to the Virtual Camera
+            virtualCamera.Follow = player.transform;
+            virtualCamera.LookAt = player.transform;
+        }
+    }
+}
--- Mimic/Assets/Scripts/SwitchControl.cs
+++ Mimic/Assets/Scripts/SwitchControl.cs
@@ -1,81 +1,78 @@
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.DualShock;
 
 public class SwitchControl : MonoBehaviour
 {
     public GameObject player; // Reference to the player GameObject
-    public GameObject enemy; // Reference to the enemy GameObject
+    public float maxRaycastDistance = 3f; // Maximum distance for the raycast
     public string[] enemyScriptsToEnable; // Script names on the enemy to enable
     public string[] enemyScriptsToDisable; // Script names on the enemy to disable
-    public float maxRaycastDistance = 3f; // Maximum distance for the raycast
-    public InputAction transformButton;
 
+    private GameObject currentEnemy; // The current enemy GameObject
     private bool playerControl = true;
     private NavMeshAgent enemyNavMeshAgent;
     private CharacterController enemyCharacterController;
     private Vector3 initialPlayerPosition;
-    private Vector3 initialEnemyPosition;
     private Quaternion initialPlayerRotation;
-    private Quaternion initialEnemyRotation;
 
     private void Start()
     {
-        transformButton.Enable();
-
-        // Ensure the enemy's Nav Mesh Agent is enabled, and Character Controller is disabled at the start
-        enemyNavMeshAgent = enemy.GetComponent<NavMeshAgent>();
-        enemyCharacterController = enemy.GetComponent<CharacterController>();
-        enemyNavMeshAgent.enabled = true;
-        enemyCharacterController.enabled = false;
-
-        // Store the initial positions and rotations
+        // Store the initial player position and rotation
         initialPlayerPosition = player.transform.position;
-        initialEnemyPosition = enemy.transform.position;
         initialPlayerRotation = player.transform.rotation;
-        initialEnemyRotation = enemy.transform.rotation;
+        currentEnemy = null; // Initialize currentEnemy as null
     }
 
     private void Update()
     {
         if (playerControl)
         {
-            if ((transformButton.WasPerformedThisFrame() || Input.GetKeyDown(KeyCode.E)) && CanSwitch())
+            if (Input.GetButtonDown("PS4_Triangle") || Input.GetKeyDown(KeyCode.E))
             {
-                // Toggle control between player and enemy
-                playerControl = !playerControl;
-
-                // Swap the visibility of player and enemy
-                player.SetActive(playerControl);
-                enemy.SetActive(!playerControl);
-
-                // Change the enemy's tag to "Player"
-                enemy.tag = "Player";
-
-                // Enable or disable selected scripts only after the switch
-                ToggleEnemyScripts();
-
-                // Enable or disable the Nav Mesh Agent and Character Controller after the switch
-                if (playerControl)
+                // Attempt to switch to a new enemy if the player is looking at one
+                if (CanSwitch(out GameObject newEnemy))
                 {
-                    enemyNavMeshAgent.enabled = true;
-                    enemyCharacterController.enabled = false;
-                }
-                else
-                {
-                    enemyNavMeshAgent.enabled = false;
-                    enemyCharacterController.enabled = true;
-                }
+                    // Toggle control between player and enemy
+                    playerControl = !playerControl;
 
-                // Set the player (invisible) to be on top of the enemy
-                player.transform.position = enemy.transform.position;
-                player.transform.rotation = enemy.transform.rotation;
+                    // Swap the visibility of player and enemy
+                    player.SetActive(playerControl);
+                    newEnemy.SetActive(!playerControl);
+
+                    // Change the enemy's tag to "Player"
+                    newEnemy.tag = "Player";
+
+                    // Enable or disable selected scripts only after the switch
+                    ToggleEnemyScripts(newEnemy);
+
+                    // Enable or disable the Nav Mesh Agent and Character Controller after the switch
+                    enemyNavMeshAgent = newEnemy.GetComponent<NavMeshAgent>();
+                    enemyCharacterController = newEnemy.GetComponent<CharacterController>();
+                    if (playerControl)
+                    {
+                        enemyNavMeshAgent.enabled = true;
+                        enemyCharacterController.enabled = false;
+                    }
+                    else
+                    {
+                        enemyNavMeshAgent.enabled = false;
+                        enemyCharacterController.enabled = true;
+                    }
+
+                    // Set the player (invisible) to be on top of the new enemy
+                    player.transform.position = newEnemy.transform.position;
+                    player.transform.rotation = newEnemy.transform.rotation;
+
+                    // Assign the new enemy as the current enemy
+                    currentEnemy = newEnemy;
+
+                    // Debug log to confirm the new enemy assignment
+                    Debug.Log("New enemy assigned: " + newEnemy.name);
+                }
             }
         }
         else
         {
-            // In this case, the enemy can switch without needing to look at anyone.
             if (Input.GetButtonDown("PS4_Triangle") || Input.GetKeyDown(KeyCode.E))
             {
                 // Toggle control between player and enemy
@@ -83,34 +80,45 @@ public class SwitchControl : MonoBehaviour
 
                 // Swap the visibility of player and enemy
                 player.SetActive(playerControl);
-                enemy.SetActive(!playerControl);
-
-                // Change the enemy's tag to "Player"
-                enemy.tag = "Player";
+                if (currentEnemy != null)
+                {
+                    currentEnemy.SetActive(!playerControl);
+                }
 
                 // Enable or disable selected scripts only after the switch
-                ToggleEnemyScripts();
+                if (currentEnemy != null)
+                {
+                    ToggleEnemyScripts(currentEnemy);
+                }
 
                 // Enable or disable the Nav Mesh Agent and Character Controller after the switch
-                if (playerControl)
+                if (currentEnemy != null)
                 {
-                    enemyNavMeshAgent.enabled = true;
-                    enemyCharacterController.enabled = false;
-                }
-                else
-                {
-                    enemyNavMeshAgent.enabled = false;
-                    enemyCharacterController.enabled = true;
+                    enemyNavMeshAgent = currentEnemy.GetComponent<NavMeshAgent>();
+                    enemyCharacterController = currentEnemy.GetComponent<CharacterController>();
+                    if (playerControl)
+                    {
+                        enemyNavMeshAgent.enabled = true;
+                        enemyCharacterController.enabled = false;
+                    }
+                    else
+                    {
+                        enemyNavMeshAgent.enabled = false;
+                        enemyCharacterController.enabled = true;
+                    }
                 }
 
                 // Set the player (invisible) to be on top of the enemy
-                player.transform.position = enemy.transform.position;
-                player.transform.rotation = enemy.transform.rotation;
+                if (currentEnemy != null)
+                {
+                    player.transform.position = currentEnemy.transform.position;
+                    player.transform.rotation = currentEnemy.transform.rotation;
+                }
             }
         }
     }
 
-    private void ToggleEnemyScripts()
+    private void ToggleEnemyScripts(GameObject enemy)
     {
         foreach (string scriptName in enemyScriptsToDisable)
         {
@@ -131,19 +139,19 @@ public class SwitchControl : MonoBehaviour
         }
     }
 
-    private bool CanSwitch()
+    private bool CanSwitch(out GameObject newEnemy)
     {
+        newEnemy = null;
+
         // Create a ray from the player's position forward
         Ray ray = new Ray(player.transform.position, player.transform.forward);
         RaycastHit hit;
 
-        // Check if the ray hits the enemy
-        if (Physics.Raycast(ray, out hit, maxRaycastDistance))
+        // Check if the ray hits an object with the "Enemy" tag
+        if (Physics.Raycast(ray, out hit, maxRaycastDistance) && hit.collider.CompareTag("Enemy"))
         {
-            if (hit.collider.gameObject == enemy)
-            {
-                return true;
-            }
+            newEnemy = hit.collider.gameObject;
+            return true;
         }
 
         return false;
