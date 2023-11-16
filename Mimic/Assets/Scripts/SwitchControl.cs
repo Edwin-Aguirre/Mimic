1f851dbecb395db498a23962ee13982fe3c8c848
Edwin-Aguirre <edwina3@uci.edu>
2023-11-15 18:57:58 -0800

    Updated Mirror Ability
    The mirror no longer uses ray casts to transform.
    
    It is much more accurate and should work when you're near an enemy.

--- Mimic/Assets/Scripts/SwitchControl.cs
+++ Mimic/Assets/Scripts/SwitchControl.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem.DualShock;
 
 public class SwitchControl : MonoBehaviour
 {
-    public GameObject player; // Reference to the player GameObject
-    public float maxRaycastDistance = 3f; // Maximum distance for the raycast
-    public string[] enemyScriptsToEnable; // Script names on the enemy to enable
-    public string[] enemyScriptsToDisable; // Script names on the enemy to disable
+    public GameObject player;
+    public float maxProximityDistance = 3f; // Maximum distance for the proximity check
+    public string[] enemyScriptsToEnable;
+    public string[] enemyScriptsToDisable;
     public InputAction transformButton;
 
-    private GameObject currentEnemy; // The current enemy GameObject
+    private GameObject currentEnemy;
     private bool playerControl = true;
     private NavMeshAgent enemyNavMeshAgent;
     private CharacterController enemyCharacterController;
@@ -22,7 +22,6 @@ public class SwitchControl : MonoBehaviour
 
     private void Start()
     {
-        // Find the player GameObject in the scene using a tag or another method
         player = GameObject.FindGameObjectWithTag("Player");
         enemySpawnSystem = FindAnyObjectByType<EnemySpawnSystem>();
 
@@ -32,13 +31,11 @@ public class SwitchControl : MonoBehaviour
             return;
         }
 
-        // Store the initial player position and rotation
         initialPlayerPosition = player.transform.position;
         initialPlayerRotation = player.transform.rotation;
-        currentEnemy = null; // Initialize currentEnemy as null
+        currentEnemy = null;
         transformButton.Enable();
     }
-
 
     private void Update()
     {
@@ -48,100 +45,95 @@ public class SwitchControl : MonoBehaviour
 
             if (transformButton.WasPerformedThisFrame())
             {
-                // Attempt to switch to a new enemy if the player is looking at one
-                if (CanSwitch(out GameObject newEnemy))
+                // Attempt to switch to a new enemy if the player is within proximity
+                if (CanSwitch())
                 {
-                    // Toggle control between player and enemy
-                    playerControl = !playerControl;
-
-                    // Swap the visibility of player and enemy
-                    player.SetActive(playerControl);
-                    newEnemy.SetActive(!playerControl);
-
-                    // Change the enemy's tag to "Player"
-                    newEnemy.tag = "Player";
-
-                    // Enable or disable selected scripts only after the switch
-                    ToggleEnemyScripts(newEnemy);
-
-                    // Enable or disable the Nav Mesh Agent and Character Controller after the switch
-                    enemyNavMeshAgent = newEnemy.GetComponent<NavMeshAgent>();
-                    enemyCharacterController = newEnemy.GetComponent<CharacterController>();
-                    if (playerControl)
+                    GameObject newEnemy = FindEnemyInProximity();
+                    if (newEnemy != null)
                     {
-                        enemyNavMeshAgent.enabled = true;
-                        enemyCharacterController.enabled = false;
+                        SwitchToEnemyCharacter(newEnemy);
                     }
-                    else
-                    {
-                        enemyNavMeshAgent.enabled = false;
-                        enemyCharacterController.enabled = true;
-                    }
-
-                    // Assign the new enemy as the current enemy
-                    currentEnemy = newEnemy;
-
-                    // Use raycast to find the AnimationControllerSwap script on the enemy
-                    AnimationControllerSwap animationControllerSwap = newEnemy.GetComponent<AnimationControllerSwap>();
-                    if (animationControllerSwap != null)
-                    {
-                        // Use the AnimationControllerSwap script to swap the Animator controller
-                        animationControllerSwap.SwapAnimatorController();
-                    }
-
-                    // Debug log to confirm the new enemy assignment
-                    Debug.Log("New enemy assigned: " + newEnemy.name);
                 }
             }
         }
         else
         {
             PlayerHealthCheck();
-            
+
             if (transformButton.WasPerformedThisFrame())
             {
-                // Check if the player is disabled before switching back
                 if (currentEnemy != null)
                 {
-                    // Set the player's position and rotation to match the enemy's position and rotation
-                    player.transform.position = currentEnemy.transform.position;
-                    player.transform.rotation = currentEnemy.transform.rotation;
-
-                    // Toggle control back to the player
-                    playerControl = !playerControl;
-
-                    // Swap the visibility of player and enemy
-                    player.SetActive(playerControl);
-                    currentEnemy.SetActive(!playerControl);
-                    Destroy(currentEnemy);
-
-                    // Change the enemy's tag back to "Enemy"
-                    currentEnemy.tag = "Enemy";
-
-                    // Enable or disable selected scripts only after the switch
-                    ToggleEnemyScripts(currentEnemy);
-
-                    // Enable or disable the Nav Mesh Agent and Character Controller after the switch
-                    enemyNavMeshAgent = currentEnemy.GetComponent<NavMeshAgent>();
-                    enemyCharacterController = currentEnemy.GetComponent<CharacterController>();
-                    if (playerControl)
-                    {
-                        enemyNavMeshAgent.enabled = true;
-                        enemyCharacterController.enabled = false;
-                    }
-                    else
-                    {
-                        enemyNavMeshAgent.enabled = false;
-                        enemyCharacterController.enabled = true;
-                    }
-
-                    // Debug log to confirm the switch back to the player
-                    Debug.Log("Switched back to player");
-
-                    enemySpawnSystem.EnemyDestroyed();
+                    SwitchBackToPlayerCharacter();
                 }
             }
         }
+    }
+
+    private GameObject FindEnemyInProximity()
+    {
+        Collider[] colliders = Physics.OverlapSphere(player.transform.position, maxProximityDistance);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                return collider.gameObject;
+            }
+        }
+
+        return null;
+    }
+
+    private void SwitchToEnemyCharacter(GameObject newEnemy)
+    {
+        playerControl = !playerControl;
+        player.SetActive(playerControl);
+        newEnemy.SetActive(!playerControl);
+        newEnemy.tag = "Player";
+        ToggleEnemyScripts(newEnemy);
+
+        enemyNavMeshAgent = newEnemy.GetComponent<NavMeshAgent>();
+        enemyCharacterController = newEnemy.GetComponent<CharacterController>();
+        if (playerControl)
+        {
+            enemyNavMeshAgent.enabled = true;
+            enemyCharacterController.enabled = false;
+        }
+        else
+        {
+            enemyNavMeshAgent.enabled = false;
+            enemyCharacterController.enabled = true;
+        }
+
+        currentEnemy = newEnemy;
+
+        AnimationControllerSwap animationControllerSwap = newEnemy.GetComponent<AnimationControllerSwap>();
+        if (animationControllerSwap != null)
+        {
+            animationControllerSwap.SwapAnimatorController();
+        }
+
+        Debug.Log("New enemy assigned: " + newEnemy.name);
+    }
+
+    private void SwitchBackToPlayerCharacter()
+    {
+        Vector3 enemyPosition = currentEnemy.transform.position;
+        Quaternion enemyRotation = currentEnemy.transform.rotation;
+
+        playerControl = true;
+        player.transform.position = enemyPosition;
+        player.transform.rotation = enemyRotation;
+        player.SetActive(true);
+        currentEnemy.SetActive(false);
+        Destroy(currentEnemy);
+        currentEnemy.tag = "Enemy";
+        ToggleEnemyScripts(player);
+        currentEnemy = null;
+
+        Debug.Log("Switched back to player");
+        enemySpawnSystem.EnemyDestroyed();
     }
 
     private void ToggleEnemyScripts(GameObject enemy)
@@ -165,22 +157,9 @@ public class SwitchControl : MonoBehaviour
         }
     }
 
-    private bool CanSwitch(out GameObject newEnemy)
+    private bool CanSwitch()
     {
-        newEnemy = null;
-
-        // Create a ray from the player's position forward
-        Ray ray = new Ray(player.transform.position, player.transform.forward);
-        RaycastHit hit;
-
-        // Check if the ray hits an object with the "Enemy" tag
-        if (Physics.Raycast(ray, out hit, maxRaycastDistance) && hit.collider.CompareTag("Enemy"))
-        {
-            newEnemy = hit.collider.gameObject;
-            return true;
-        }
-
-        return false;
+        return FindEnemyInProximity() != null;
     }
 
     private void PlayerHealthCheck()
@@ -191,36 +170,8 @@ public class SwitchControl : MonoBehaviour
         if (characterHealthSystem != null && characterHealthSystem.currentHealth <= 0 && gameObject.name != "Player")
         {
             Debug.Log("Character's health reached zero. Switching back to the player.");
-            SwitchToPlayerCharacter();
+            SwitchBackToPlayerCharacter();
             enemySpawnSystem.EnemyDestroyed();
-        }
-    }
-
-    private void SwitchToPlayerCharacter()
-    {
-        if (!playerControl)
-        {
-            // Store the enemy's position and rotation before switching
-            Vector3 enemyPosition = currentEnemy.transform.position;
-            Quaternion enemyRotation = currentEnemy.transform.rotation;
-
-            // Toggle control back to the player
-            playerControl = true;
-
-            // Set the player's position and rotation to match the enemy's
-            player.transform.position = enemyPosition;
-            player.transform.rotation = enemyRotation;
-
-            // Swap the visibility of player and enemy
-            player.SetActive(true);
-            currentEnemy.SetActive(false);
-            Destroy(currentEnemy);
-
-            // Reset the currentEnemy reference
-            currentEnemy = null;
-
-            // Debug log to confirm the switch back to the player
-            Debug.Log("Switched back to player");
         }
     }
 }
