fed6b58fc23bb5096449fc6430186d925e5c3256
Edwin-Aguirre <edwina3@uci.edu>
2024-03-07 19:15:35 -0800

    Added Controls UI
    You can now see what the controls are in the escape screen.

--- Mimic/Assets/Scripts/EscapeScreen.cs
+++ Mimic/Assets/Scripts/EscapeScreen.cs
@@ -4,18 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.DualShock;
+using UnityEngine.InputSystem.XInput;
 
 public class EscapeScreen : MonoBehaviour
 {
     [SerializeField] private GameObject escapeScreen;
     [SerializeField] private GameObject optionsScreen;
     [SerializeField] private GameObject optionsButton;
+    [SerializeField] private GameObject controlsScreen;
+    [SerializeField] private GameObject controlsButton;
     [SerializeField] private GameObject resolutionDropdown;
+    [SerializeField] private GameObject keyboardPanel;
+    [SerializeField] private GameObject playstationPanel;
+    [SerializeField] private GameObject xboxPanel;
     [SerializeField] private InputAction escapeButton;
 
     [SerializeField] private string scriptsToDisable; // Names of scripts to disable, separated by commas
 
     private bool isGamePaused = false;
+
+    public enum InputType { PC, PlayStation, Xbox }
+    public InputType currentInput = InputType.PC; // Default to PC
+
+    private bool playerNearObject = false;
+
+    private string disableController;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +41,8 @@ public class EscapeScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SetPlatformPanel();
+
         if (escapeButton.WasPressedThisFrame())
         {
             if (isGamePaused)
@@ -44,6 +60,7 @@ public class EscapeScreen : MonoBehaviour
     {
         escapeScreen.SetActive(false);
         optionsScreen.SetActive(false);
+        controlsScreen.SetActive(false);
         isGamePaused = false;
         Cursor.visible = false;
         EnableScripts(scriptsToDisable);
@@ -65,6 +82,56 @@ public class EscapeScreen : MonoBehaviour
         Cursor.visible = true;
         EventSystem.current.SetSelectedGameObject(resolutionDropdown);
         DisableScripts(scriptsToDisable);
+    }
+
+    public void onClickControlsButton()
+    {
+        controlsScreen.SetActive(true);
+        escapeScreen.SetActive(false);
+        Cursor.visible = true;
+        DisableScripts(scriptsToDisable);
+    }
+
+    void SetPlatformPanel()
+    {
+        var gamepad = Gamepad.current;
+
+        if (escapeButton.WasPerformedThisFrame())
+        {
+            currentInput = InputType.PC;
+        } 
+        if (gamepad is DualShockGamepad && escapeButton.WasPerformedThisFrame())
+        {
+            currentInput = InputType.PlayStation;
+            disableController = "DualShock4GamepadHID";
+        }
+        if (gamepad is XInputController && gamepad.name == disableController && escapeButton.WasPerformedThisFrame())
+        {
+            currentInput = InputType.Xbox;
+        }
+        else if (gamepad is XInputController)
+        {
+            disableController = "XInputControllerWindows";
+        }
+        
+        switch (currentInput)
+        {
+            case InputType.PC:
+                keyboardPanel.SetActive(true);
+                playstationPanel.SetActive(false);
+                xboxPanel.SetActive(false);
+                break;
+            case InputType.PlayStation:
+                playstationPanel.SetActive(true);
+                keyboardPanel.SetActive(false);
+                xboxPanel.SetActive(false);
+                break;
+            case InputType.Xbox:
+                xboxPanel.SetActive(true);
+                keyboardPanel.SetActive(false);
+                playstationPanel.SetActive(false);
+                break;
+        }
     }
 
     // Disable scripts on game objects with the specified name
